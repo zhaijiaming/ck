dc325c48cf4f46795293f64fa7e7eb601428bf2e
yaowenhua@jhchengkang.cn <yaowenhua@jhchengkang.cn>
2016-09-27 09:16:04 +0800

    rpt demo

---
+++ CKWMS/reports/ReportZY.aspx.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using CKWMS.BSL;
+using CKWMS.EFModels;
+using CKWMS.BSL;
+
+namespace CKWMS.reports
+{
+    public partial class ReportZY : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                int _userid = (int)Session["user_id"];
+                string name = "";
+                int i = 0;
+                ReportDataSet _rds = new ReportDataSet();
+                //DataTable _dt;                
+                if (Request.QueryString["pid"] != null)
+                {
+                    name = Request.QueryString["pid"];
+                    switch (name)
+                    {
+                        case "daviskw":
+                            rptView.Reset();
+                            rptView.LocalReport.ReportPath = "reports/daviskw.rdlc";
+                            rptView.LocalReport.DataSources.Clear();
+                            DataTable dtkw = _rds.Tables["davistest-kw"];
+                            var _kws = ServiceFactory.wms_kuweiservice.LoadEntities(p => p.IsDelete == false);
+                            DataRow drkw;
+                            foreach (wms_kuwei _pr in _kws)
+                            {
+                                drkw = dtkw.NewRow();
+                                drkw["kuwei"] = _pr.Mingcheng;
+                                drkw["huojia"] = _pr.Huojia;
+                                drkw["lei"] = _pr.Lieshu;
+                                drkw["ceng"] = _pr.Censhu;
+                                dtkw.Rows.Add(drkw);
+                            }
+                            rptView.LocalReport.DataSources.Add(new Microsoft.Reporting.WebForms.ReportDataSource("DataSet1", _rds.Tables["davistest-kw"]));
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+
+        }
+    }
+}
