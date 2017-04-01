3c54adee549fa6be26ec8594e531dfafaeb71cfe
yaowenhua@jhchengkang.cn <yaowenhua@jhchengkang.cn>
2017-04-01 17:34:25 +0800

    in,out report add

---
+++ CKWMS/Controllers/wms_inreportController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Linq.Expressions;
+using X.PagedList;
+using CKWMS.EFModels;
+using CKWMS.IBSL;
+using CKWMS.BSL;
+using CKWMS.Common;
+
+namespace CKWMS.Controllers
+{
+    public class wms_inreportController : Controller
+    {
+        // GET: wms_inreport
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult GetInList(string page)
+        {
+            if (string.IsNullOrEmpty(page))
+                page = "1";
+            int userid = (int)Session["user_id"];
+            string pagetag = "wms_inreport_list";
+            Expression<Func<wms_recievelist_v, bool>> where = PredicateExtensionses.True<wms_recievelist_v>();
+            searchcondition sc = searchconditionService.GetInstance().GetEntityById(searchcondition => searchcondition.UserID == userid && searchcondition.PageBrief == pagetag);
+            if (sc != null && sc.ConditionInfo != null)
+            {
+                string[] sclist = sc.ConditionInfo.Split(';');
+                foreach (string scl in sclist)
+                {
+                    string[] scld = scl.Split(',');
+                    switch (scld[0])
+                    {
+                        case "RukuBH":
+                            string CPMC = scld[1];
+                            string CPMCequal = scld[2];
+                            string CPMCand = scld[3];
+                            if (!string.IsNullOrEmpty(CPMC))
+                            {
+                                if (CPMCequal.Equals("="))
+                                {
+                                    if (CPMCand.Equals("and"))
+                                        where = where.And(p => p.RukudanBH == CPMC.Trim());
+                                    else
+                                        where = where.Or(p => p.RukudanBH == CPMC.Trim());
+                                }
+                                if (CPMCequal.Equals("like"))
+                                {
+                                    if (CPMCand.Equals("and"))
+                                        where = where.And(p => p.RukudanBH.Contains(CPMC.Trim()));
+                                    else
+                                        where = where.Or(p => p.RukudanBH.Contains(CPMC.Trim()));
+                                }
+                            }
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                ViewBag.SearchCondition = sc.ConditionInfo;
+            }
+
+            var tempData = ServiceFactory.wms_rukudanservice.GetInList(where.Compile()).ToPagedList<wms_recievelist_v>(int.Parse(page), int.Parse(System.Web.Configuration.WebConfigurationManager.AppSettings["ShowPerPage"]));
+            ViewBag.inreport = tempData;
+            return View(tempData);
+        }
+    }
+}
---
+++ CKWMS/Controllers/wms_outreportController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Linq.Expressions;
+using X.PagedList;
+using System.Web.Mvc;
+using CKWMS.EFModels;
+using CKWMS.IBSL;
+using CKWMS.BSL;
+using CKWMS.Models;
+using CKWMS.Common;
+namespace CKWMS.Controllers
+{
+    public class wms_outreportController : Controller
+    {
+        // GET: wms_outreport
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult GetOutList(string page)
+        {
+            if (string.IsNullOrEmpty(page))
+                page = "1";
+            int userid = (int)Session["user_id"];
+            string pagetag = "wms_outreport_list";
+            Expression<Func<wms_outdetaillist_v, bool>> where = PredicateExtensionses.True<wms_outdetaillist_v>();
+            searchcondition sc = searchconditionService.GetInstance().GetEntityById(searchcondition => searchcondition.UserID == userid && searchcondition.PageBrief == pagetag);
+            if (sc != null && sc.ConditionInfo != null)
+            {
+                string[] sclist = sc.ConditionInfo.Split(';');
+                foreach (string scl in sclist)
+                {
+                    string[] scld = scl.Split(',');
+                    switch (scld[0])
+                    {
+                        case "ChukuBH":
+                            string CPMC = scld[1];
+                            string CPMCequal = scld[2];
+                            string CPMCand = scld[3];
+                            if (!string.IsNullOrEmpty(CPMC))
+                            {
+                                if (CPMCequal.Equals("="))
+                                {
+                                    if (CPMCand.Equals("and"))
+                                        where = where.And(p => p.ChukudanBH == CPMC.Trim());
+                                    else
+                                        where = where.Or(p => p.ChukudanBH == CPMC.Trim());
+                                }
+                                if (CPMCequal.Equals("like"))
+                                {
+                                    if (CPMCand.Equals("and"))
+                                        where = where.And(p => p.ChukudanBH.Contains(CPMC.Trim()));
+                                    else
+                                        where = where.Or(p => p.ChukudanBH.Contains(CPMC.Trim()));
+                                }
+                            }
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                ViewBag.SearchCondition = sc.ConditionInfo;
+            }
+
+            var tempData = ServiceFactory.wms_chukudanservice.GetOutList(where.Compile()).ToPagedList<wms_outdetaillist_v>(int.Parse(page), int.Parse(System.Web.Configuration.WebConfigurationManager.AppSettings["ShowPerPage"]));
+            ViewBag.outreport = tempData;
+            return View(tempData);
+        }
+    }
+}
